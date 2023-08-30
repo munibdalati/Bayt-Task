8db4d338fa3752e30226e1487659b44272fb34ad
munib dalati <munib.dalati@gmail.com>
2023-08-30 15:17:17 +0300

    fixed nav bar

--- Assignment1/js/script.js
+++ Assignment1/js/script.js
@@ -1,8 +1,21 @@
-const hamburger =document.querySelector(".hamburger");
-const navMenu =document.querySelector(".nav-menu");
+//hamburger action in the navbar
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav__menu");
 
-hamburger.addEventListener("click",()=>{
-    hamburger.classList.toggle("active")
-    navMenu.classList.toggle("active")
-})
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
+// up arrow action
+const upArrow = document.querySelector(".uparrow__icon");
+
+upArrow.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  // Scroll to the top of the page with smooth behavior
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
