2025a3046c416f5e213b35245504f87c29e82341
munib dalati <munib.dalati@gmail.com>
2023-08-31 11:42:46 +0300

    updage js

--- Assignment1/js/script.js
+++ Assignment1/js/script.js
@@ -3,7 +3,6 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav__menu");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
 
@@ -11,7 +10,6 @@ hamburger.addEventListener("click", () => {
 const upArrow = document.querySelector(".uparrow__icon");
 
 upArrow.addEventListener("click", function (event) {
-  event.preventDefault();
 
   // Scroll to the top of the page with smooth behavior
   window.scrollTo({
